c593af3559adfa2a1ee5058c91bb8034d8acaa61
majid <majid.kamal@virtual-base.com>
2015-01-07 18:14:02 +0500

    testing

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -3,11 +3,15 @@
 
 @section('head')
 	@parent
+<<<<<<< HEAD
 
     <title>Test Project hello worlddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd ddddddddddddddddddd dddddddddddddddddddd</title>
 
     <title>Test Project khkjhskjfhkjfhksjfh jsghfjksghfkjsgfkjh </title>
 
+=======
+    <title>Test Project hello worlddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd ddddddddddddddddddd dddddddddddddddddddd</title>
+>>>>>>> 1a8d1e1... first Hello world pushed
 @stop
 
 
